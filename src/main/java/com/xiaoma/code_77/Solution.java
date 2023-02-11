ab95151548075a94c58099c165b35631041bc7b5
xiaoma <15234829838@163.com>
2023-02-11 16:03:54 +0800

    2.code-77-->组合问题--xiaoma

--- src/main/java/com/xiaoma/code_77/Solution.java
+++ src/main/java/com/xiaoma/code_77/Solution.java
@@ -26,6 +26,8 @@ class Solution {
         }
         // 这里进行了剪枝操作
         // 这里 i <= n - (k - path.size()) + 1 不是很清楚什么意思
+        // k - path.size() ==> 还剩下需要排列的个数
+        // i + (k - path.size()) - 1 ==> 当前进行的数加上还需要排列的数 - 1 如果小于等于n则说明可以进行下面操作
         for (int i = startIndex; i <= n - (k - path.size()) + 1; i ++ ) {
             path.add(i);
             combineHelper(n , k , i + 1);
@@ -47,6 +49,11 @@ class Solution {
 
     @Test
     public void test01() {
-
+        new Solution().combine(4 , 4).forEach(item -> {
+            item.forEach(k -> {
+                System.out.print(k + " ");
+            });
+            System.out.println();
+        });
     }
 }
